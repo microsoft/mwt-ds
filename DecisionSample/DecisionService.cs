f2259789edbec7c9eaf663f4e57a4414a4d7c0ca
Luong Hoang <lhoang@live.com>
2015-01-12 17:29:24 -0500

    refactoring & added comments

--- DecisionSample/DecisionService.cs
+++ DecisionSample/DecisionService.cs
@@ -10,6 +10,49 @@ using Microsoft.WindowsAzure.Storage;
 namespace DecisionSample
 {
     /// <summary>
+    /// Configuration object for the client decision service which contains settings for batching, retry storage, etc...
+    /// </summary>
+    class DecisionServiceConfiguration<TContext>
+    {
+        public DecisionServiceConfiguration()
+        {
+            ContextJsonSerializer = x => JsonConvert.SerializeObject(x);
+
+            BatchConfig = new BatchingConfiguration()
+            {
+                BufferSize = 4 * 1024 * 1024,
+                Duration = TimeSpan.FromMinutes(1),
+                EventCount = 10000
+            };
+        }
+        public Func<TContext, string> ContextJsonSerializer { get; set; }
+        public IRetryStorage RetryStorage { get; set; }
+        public BatchingConfiguration BatchConfig { get; set; }
+    }
+
+    /// <summary>
+    /// Encapsulates logic for recorder with async server communications & policy update.
+    /// </summary>
+    class DecisionService<TContext> : IDisposable
+    {
+        public DecisionService(DecisionServiceConfiguration<TContext> config)
+        {
+            recorder = new DecisionServiceRecorder<TContext>(config.RetryStorage, config.BatchConfig);
+            policy = new DecisionServicePolicy<TContext>();
+        }
+        public void ReportOutcome(string outcomeJson, string uniqueKey)
+        {
+            recorder.ReportOutcome(outcomeJson, uniqueKey);
+        }
+
+        public IRecorder<TContext> Recorder { get { return recorder; } }
+        public IPolicy<TContext> Policy { get { return policy; } }
+
+        private DecisionServiceRecorder<TContext> recorder;
+        private DecisionServicePolicy<TContext> policy;
+    }
+
+    /// <summary>
     /// Represents a collection of batching criteria.  
     /// </summary>
     /// <remarks>
@@ -33,17 +76,26 @@ namespace DecisionSample
         public ulong BufferSize { get; set; }
     }
 
+    /// <summary>
+    /// Represents a retry storage.
+    /// </summary>
     interface IRetryStorage
     {
         // TODO: Should storage size have an upper limit?
         // long GetMaxSize();
     }
 
+    /// <summary>
+    /// Physical disk retry storage 
+    /// </summary>
     class DiskRetryStorage : IRetryStorage
     {
         public DiskRetryStorage(string filePath) { }
     }
 
+    /// <summary>
+    /// Azure Blob retry storage
+    /// </summary>
     class AzureBlobRetryStorage : IRetryStorage
     {
         public AzureBlobRetryStorage(CloudStorageAccount authorizationObject) { }
@@ -74,41 +126,5 @@ namespace DecisionSample
     internal class DecisionServicePolicy<TContext> : IPolicy<TContext>, IDisposable
     { 
         // Recorder should talk to the Policy to pass over latest model version
-    }
-
-    class DecisionServiceConfiguration<TContext>
-    {
-        public DecisionServiceConfiguration()
-        {
-            ContextJsonSerializer = x => JsonConvert.SerializeObject(x);
-
-            BatchConfig = new BatchingConfiguration() { 
-                BufferSize = 4 * 1024 * 1024,
-                Duration = TimeSpan.FromMinutes(1),
-                EventCount = 10000
-            };
-        }
-        public Func<TContext, string> ContextJsonSerializer { get; set; }
-        public IRetryStorage RetryStorage { get; set; }
-        public BatchingConfiguration BatchConfig { get; set; }
-    }
-
-    class DecisionService<TContext> : IDisposable
-    {
-        public IRecorder<TContext> Recorder { get { return recorder; } }
-        public IPolicy<TContext> Policy { get { return policy; } }
-
-        public DecisionService(DecisionServiceConfiguration<TContext> config)
-        {
-            recorder = new DecisionServiceRecorder<TContext>(config.RetryStorage, config.BatchConfig);
-            policy = new DecisionServicePolicy<TContext>();
-        }
-        public void ReportOutcome(string outcomeJson, string uniqueKey) 
-        {
-            recorder.ReportOutcome(outcomeJson, uniqueKey);
-        }
-
-        private DecisionServiceRecorder<TContext> recorder;
-        private DecisionServicePolicy<TContext> policy;
     }
 }
