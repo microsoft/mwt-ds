4e93779b59b41a62f6ad399ec15f57962182833d
Luong Hoang <lhoang@live.com>
2015-03-03 19:08:07 -0500

    added sample .net 4.5.2 mvc site that uses decision service

---
+++ ClientDecisionServiceWebSample/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using ClientDecisionServiceWebSample.Extensions;
+using System.Web.Hosting;
+using System.Web.Mvc;
+
+namespace ClientDecisionServiceWebSample.Controllers
+{
+    public class HomeController : AsyncController
+    {
+        static int requestCount = 0;
+        static readonly string exploreFile = "dsexplore.txt";
+
+        public ActionResult Index()
+        {
+            if (requestCount == 0)
+            {
+                System.IO.File.Delete(exploreFile);
+            }
+            requestCount++;
+
+            HostingEnvironment.QueueBackgroundWorkItem(token => {
+                DecisionServiceWrapper<string>.Create();
+
+                string context = "context " + requestCount;
+                string outcome = "outcome " + requestCount;
+
+                uint action = DecisionServiceWrapper<string>.Service.ChooseAction(requestCount.ToString(), context);
+                DecisionServiceWrapper<string>.Service.ReportOutcome(outcome, requestCount.ToString());
+
+                System.IO.File.AppendAllLines(exploreFile, new string[] { "Action: " + action.ToString() });
+            });
+
+            string explorationData = System.IO.File.Exists(exploreFile) ? string.Join("<br />", System.IO.File.ReadAllLines(exploreFile)) : string.Empty;
+
+            return Content(string.Format("Ok. Request: {0}. Exploration: {1}", requestCount, explorationData));
+        }
+    }
+}
---
+++ ClientDecisionServiceWebSample/Extensions/DecisionServiceWrapper.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MultiWorldTesting;
+using ClientDecisionService;
+
+namespace ClientDecisionServiceWebSample.Extensions
+{
+    public static class DecisionServiceWrapper<TContext>
+    {
+        public static EpsilonGreedyExplorer<TContext> Explorer { get; set; }
+        public static DecisionServiceConfiguration<TContext> Configuration { get; set; }
+        public static DecisionService<TContext> Service { get; set; }
+
+        public static void Create()
+        {
+            if (Explorer == null)
+            {
+                Explorer = new EpsilonGreedyExplorer<TContext>(new MartPolicy<TContext>(), .2f, 10);
+            }
+
+            if (Configuration == null)
+            {
+                //Configuration = new DecisionServiceConfiguration<TContext>("rcvtest", "c01ff675-5710-4814-a961-d03d2d6bce65", Explorer)
+                Configuration = new DecisionServiceConfiguration<TContext>("louiemart", "c7b77291-f267-43da-8cc3-7df7ec2aeb06", Explorer)
+                {
+                    BatchConfig = new BatchingConfiguration 
+                    {
+                        MaxDuration = TimeSpan.FromSeconds(5),
+                        MaxBufferSizeInBytes = 10,
+                        MaxEventCount = 1,
+                        MaxUploadQueueCapacity = 1,
+                        UploadRetryPolicy = BatchUploadRetryPolicy.Retry
+                    }
+                };
+            }
+
+            if (Service == null)
+            {
+                Service = new DecisionService<TContext>(Configuration);
+            }
+        }
+    }
+
+    class MartPolicy<TContext> : IPolicy<TContext>
+    {
+        public uint ChooseAction(TContext context)
+        {
+            return 5;
+        }
+    }
+}
