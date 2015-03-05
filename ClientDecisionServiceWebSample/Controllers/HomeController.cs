da5b4d15ce5843c8d1bafce9eb6993dc4171ef8a
Luong Hoang <lhoang@live.com>
2015-03-05 18:10:31 -0500

    added retraining code to sample web project

--- ClientDecisionServiceWebSample/Controllers/HomeController.cs
+++ ClientDecisionServiceWebSample/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
 ﻿using ClientDecisionServiceWebSample.Extensions;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
+using System.Net.Http;
 using System.Web.Hosting;
 using System.Web.Mvc;
 
@@ -20,10 +23,21 @@ namespace ClientDecisionServiceWebSample.Controllers
             }
             requestCount++;
 
-            HostingEnvironment.QueueBackgroundWorkItem(token => {
+            string appToken = "c7b77291-f267-43da-8cc3-7df7ec2aeb06";
+
+            HostingEnvironment.QueueBackgroundWorkItem(cancelToken =>
+            {
+                while (!cancelToken.IsCancellationRequested)
+                {
+                    cancelToken.WaitHandle.WaitOne(5000);
+                    HomeController.RetrainModel(appToken, numberOfActions: 2);
+                }
+            });
+
+            HostingEnvironment.QueueBackgroundWorkItem(cancelToken => {
                 DecisionServiceWrapper<string>.Create(
-                    appId: "louiemart", 
-                    appToken: "c7b77291-f267-43da-8cc3-7df7ec2aeb06", 
+                    appId: "louiemart",
+                    appToken: appToken, 
                     epsilon: .2f, 
                     numActions: 10,
                     modelOutputDir: outputDir);
@@ -40,6 +54,42 @@ namespace ClientDecisionServiceWebSample.Controllers
             string explorationData = System.IO.File.Exists(exploreFile) ? string.Join("<br />", System.IO.File.ReadAllLines(exploreFile)) : string.Empty;
 
             return Content(string.Format("Ok. Request: {0}. Exploration: {1}", requestCount, explorationData));
+        }
+
+        static void RetrainModel(string appToken, int numberOfActions)
+        {
+            using (var client = new HttpClient())
+            {
+                var values = new Dictionary<string, string>
+                {
+                    { "token", appToken },
+                    { "numberOfActions", numberOfActions.ToString() }
+                };
+
+                var content = new FormUrlEncodedContent(values);
+
+                var responseTask = client.PostAsync(
+                    "http://mwtds.azurewebsites.net//Application/RetrainModel",
+                    content
+                );
+                responseTask.Wait();
+
+                var response = responseTask.Result;
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    var t2 = response.Content.ReadAsStringAsync();
+                    t2.Wait();
+                    Trace.TraceError("Failed to retrain model.");
+                    Trace.WriteLine(t2.Result);
+                    Trace.WriteLine(response.ReasonPhrase);
+                    Trace.WriteLine(response.Headers.ToString());
+                }
+                else
+                {
+                    Trace.TraceInformation("Retraining model success.");
+                }
+            }
         }
     }
 }
