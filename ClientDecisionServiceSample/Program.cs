dd035dfdca45f9bda5db67f6820e51fec0dc3be6
Luong Hoang <lhoang@live.com>
2015-03-18 16:28:52 -0400

    added sample code to test azureml bes service for model retraining.

--- ClientDecisionServiceSample/Program.cs
+++ ClientDecisionServiceSample/Program.cs
@@ -1,18 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Dynamic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
+﻿using ClientDecisionService;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
 using MultiWorldTesting;
 using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
 using System.Threading.Tasks.Dataflow;
-using Microsoft.WindowsAzure.Storage;
-using Microsoft.WindowsAzure.Storage.Queue;
-using ClientDecisionService;
 
 namespace ClientDecisionServiceSample
 {
@@ -29,6 +29,134 @@ namespace ClientDecisionServiceSample
             catch (Exception e)
             {
                 Console.WriteLine("Exception: " + e.Message + " " + e.StackTrace);
+            }
+        }
+
+        private static void TestAzure()
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=decisionservice;AccountKey=hAv42cl/DlFLwd+N23/wNQKub/nVSEYyO6zjlksgMFC9/HVhQMHpNVhdaZGTD1PT0W7lqfKbf9LVt2/z2K3Quw==");
+
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            var containers = blobClient.ListContainers("testcomplete");
+
+            var parallelOptions = new ParallelOptions { MaxDegreeOfParallelism = Environment.ProcessorCount * 2 };
+            foreach (var c in containers)
+            {
+                Parallel.ForEach(c.ListBlobs(), parallelOptions, x => ((CloudBlockBlob)x).Delete());
+            }
+
+            CloudBlobContainer blobContainer = blobClient.GetContainerReference("complete");
+
+            CloudBlockBlob blockBlob = blobContainer.GetBlockBlobReference("");
+
+            DateTimeOffset lastBlobDate = new DateTimeOffset();
+            IEnumerable<IListBlobItem> blobs = blobContainer.ListBlobs();
+            foreach (IListBlobItem blobItem in blobs)
+            {
+                if (blobItem is CloudBlockBlob)
+                {
+                    var bbItem = (CloudBlockBlob)blobItem;
+                    DateTimeOffset bbDate = bbItem.Properties.LastModified.GetValueOrDefault();
+                    if (bbDate >= lastBlobDate)
+                    {
+                        blockBlob = bbItem;
+                        lastBlobDate = bbDate;
+                    }
+                }
+            }
+        }
+
+        private static void RetrainModel(string token, int numberOfActions)
+        {
+            // Int 
+            string apiKey = "TmGRqNMWIZd9b9dDvXSF8AtK5t1NjCnxVB5Jm4QWH+AmAZf8YBFVMPrc3w+yqtW9hFf/hK6RhtdkfrRrCkmh5Q==";
+            string baseAddress = "https://ussouthcentral.services.azureml-int.net/";
+            string postAddress = "workspaces/6c6831ae655346a5be7cc6c9bb9dfefc/services/a2d00c3ceb2e414785ce707483327d54/jobs";
+            string statusAddress = "workspaces/6c6831ae655346a5be7cc6c9bb9dfefc/services/a2d00c3ceb2e414785ce707483327d54/jobs/{0}?api-version=2.0";
+
+            // Afxcurrent
+            //string apiKey = "RCqaC3h/dKl9iAn75wefSSDeY67H2y3raHnRfvKOxWCKyYdOceBkVN/mfEn5f9yY0StliDYSifDO2MtTdkpUHw==";
+            //string baseAddress = "https://afxcurrentrrs.cloudapp.net/";
+            //string postAddress = "workspaces/2f3007c334664c069b1d1841166dc2d6/services/bdb537033b5d44869909c8e14e7fa9b4/jobs";
+            //string statusAddress = "workspaces/2f3007c334664c069b1d1841166dc2d6/services/bdb537033b5d44869909c8e14e7fa9b4/jobs/{0}?api-version=2.0";
+            //ServicePointManager.ServerCertificateValidationCallback += (a, b, c, d) => true;
+
+            string jobId = string.Empty;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseAddress);
+                client.Timeout = TimeSpan.FromMinutes(5);
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
+
+                if (String.IsNullOrWhiteSpace(jobId))
+                {
+                    var besInput = new AzureMLBESInputType
+                    {
+                        Input = null,
+                        //Input = new AzureMLBESInputType.InputType
+                        //{
+                        //    ConnectionString = "DefaultEndpointsProtocol=https;AccountName=louiemartstorage;AccountKey=NQ7fHHuwvSnTERMNLAxGoz0Nf9LzV7Jl7651KhA1T3AB/i+/wTArWbV1TKowflIMEca6A53JkcwRsrind8P6+g==",
+                        //    RelativeLocation = "dummy/dummy.csv",
+                        //    SasBlobToken = null,
+                        //    BaseLocation = null,
+                        //},
+                        Output = null,
+                        GlobalParameters = new AzureMLBESInputType.GlobalParametersType
+                        {
+                            ReaderToken = token,
+                            Token = token,
+                            NumberOfActions = numberOfActions
+                        }
+                    };
+
+                    Task<HttpResponseMessage> responseTask = client.PostAsync(
+                        postAddress,
+                        new StringContent(JsonConvert.SerializeObject(besInput), Encoding.UTF8, "application/json")
+                    );
+                    responseTask.Wait();
+
+                    HttpResponseMessage response = responseTask.Result;
+                    var t2 = response.Content.ReadAsStringAsync();
+                    t2.Wait();
+
+                    Console.WriteLine("REQUEST");
+                    Console.WriteLine(t2.Result);
+                    Console.WriteLine(response.Headers.ToString());
+
+                    if (!response.IsSuccessStatusCode)
+                    {
+
+                        Trace.TraceError("Failed to retrain a new model through AzureML's batch execution service.");
+                        Trace.WriteLine(t2.Result);
+                        Trace.WriteLine(response.ReasonPhrase);
+                        Trace.WriteLine(response.Headers.ToString());
+                    }
+                    else
+                    {
+                        Trace.TraceInformation("Model retraining succeeded");
+                    }
+
+                    jobId = t2.Result.Substring(1, t2.Result.Length - 2);
+                }
+
+                Console.WriteLine("POLLING RESULT");
+                for (int i = 0; i < 20; i++)
+                {
+                    string getStatusAddress = string.Format(statusAddress, jobId);
+
+                    var responseTask2 = client.GetAsync(getStatusAddress);
+                    responseTask2.Wait();
+                    var response2 = responseTask2.Result;
+
+                    var t = response2.Content.ReadAsStringAsync();
+                    t.Wait();
+
+                    Console.WriteLine(t.Result);
+                    Console.WriteLine(response2.Headers.ToString());
+
+                    System.Threading.Thread.Sleep(2000);
+                }
+                
             }
         }
 
@@ -99,10 +227,10 @@ namespace ClientDecisionServiceSample
             }
 
             var serviceConfig = new DecisionServiceConfiguration<UserContext>(
-                appId: "rcvtest",
-                authorizationToken: "c01ff675-5710-4814-a961-d03d2d6bce65",
-                //appId: "louiemart",
-                //authorizationToken: "c7b77291-f267-43da-8cc3-7df7ec2aeb06",
+                //appId: "rcvtest",
+                //authorizationToken: "c01ff675-5710-4814-a961-d03d2d6bce65",
+                appId: "louiemart",
+                authorizationToken: "10198550-a074-4f9c-8b15-cc389bc2bbbe",
                 explorer: new EpsilonGreedyExplorer<UserContext>(new UserPolicy(), epsilon: 0.2f, numActions: 2))
             {
                 BatchConfig = new BatchingConfiguration()
@@ -246,6 +374,33 @@ namespace ClientDecisionServiceSample
         public List<float> ScoreActions(UserContext context)
         {
             return new List<float>();
+        }
+    }
+
+    public class AzureMLBESInputType
+    {
+        public InputType Input { get; set; }
+        public string Output { get; set; }
+        public GlobalParametersType GlobalParameters { get; set; }
+
+        public class InputType
+        {
+            public string ConnectionString { get; set; }
+            public string RelativeLocation { get; set; }
+            public string BaseLocation { get; set; }
+            public string SasBlobToken { get; set; }
+        }
+
+        public class GlobalParametersType
+        {
+            [JsonProperty(PropertyName = "Authorization Token")]
+            public string ReaderToken { get; set; }
+
+            [JsonProperty(PropertyName = "Decision Service Authorization Token")]
+            public string Token { get; set; }
+
+            [JsonProperty(PropertyName = "Number of actions")]
+            public int NumberOfActions { get; set; }
         }
     }
 }
