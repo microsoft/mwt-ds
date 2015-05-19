332204188ace08442b49a1ac379635a57d3f6c99
Luong Hoang <lhoang@live.com>
2015-05-19 15:34:06 -0400

    added command center constants to contract

--- MultiWorldTestingServiceContract/ServiceConstants.cs
+++ MultiWorldTestingServiceContract/ServiceConstants.cs
@@ -8,6 +8,12 @@ namespace Microsoft.Research.MultiWorldTesting.Contract
 {
     public static class ServiceConstants
     {
+        // Command Center
+        public static readonly string CommandCenterAddress = "https://mwtds.azurewebsites.net";
+        public static readonly string CommandCenterApplicationAddress = CommandCenterAddress + "/Application";
+        public static readonly string CommandCenterGetMetadataAddress = CommandCenterApplicationAddress + "/GetMetadata?token={0}";
+
+        // Join Server
         public static readonly string JoinAddress = "http://decisionservice.cloudapp.net";
         public static readonly string JoinPostAddress = "/join";
         public static readonly string TokenAuthenticationScheme = "Bearer";
