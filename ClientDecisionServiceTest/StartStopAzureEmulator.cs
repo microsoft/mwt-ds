d2d856e52cdfa2c9792e5ebc77f316204ec5c6a2
Luong Hoang <lhoang@live.com>
2016-02-02 16:48:54 -0500

    updated vw nuget, added code to auto start/stop azure emulator

---
+++ ClientDecisionServiceTest/StartStopAzureEmulator.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientDecisionServiceTest
+{
+    [TestClass]
+    public sealed class StartStopAzureEmulator
+    {
+        private static bool _wasUp;
+        
+        [AssemblyInitialize]
+        public static void StartAzureBeforeAllTestsIfNotUp(Microsoft.VisualStudio.TestTools.UnitTesting.TestContext context)
+        {
+            if (!AzureStorageEmulatorManager.IsProcessStarted())
+            {
+                AzureStorageEmulatorManager.StartStorageEmulator();
+                _wasUp = false;
+            }
+            else
+            {
+                _wasUp = true;
+            }
+
+        }
+
+        [AssemblyCleanup]
+        public static void StopAzureAfterAllTestsIfWasDown()
+        {
+            if (!_wasUp)
+            {
+                AzureStorageEmulatorManager.StopStorageEmulator();
+            }
+            else
+            {
+                // Leave as it was before testing...
+            }
+        }
+    }
+}
