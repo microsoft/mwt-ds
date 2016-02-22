df3f9fffa439428e76c5ca3b4e8ec1531095bd9e
Luong Hoang <lhoang@live.com>
2016-02-22 12:25:16 -0500

    added support for multi-token usage via static memcache. added .config transformation to auto register and dispose these objects via IHttpModule as suggested by @marcozo

---
+++ ClientDecisionService/DecisionServiceHttpModule.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+using System.Web;
+
+namespace Microsoft.Research.MultiWorldTesting.ClientLibrary
+{
+    [EditorBrowsable(EditorBrowsableState.Never)] // Hide this from VS code browsing
+    class DecisionServiceHttpModule : IHttpModule
+    {
+        public void Init(HttpApplication context)
+        { 
+            /* do nothing */
+            Trace.TraceInformation("Decision Service Cache detected Init event.");
+        }
+
+        public void Dispose()
+        {
+            DecisionServiceFactory.EvictAll();
+            Trace.TraceInformation("Decision Service Cache detected Dispose event.");
+        }
+    }
+}
