15abe2f962347c33ccc8534122dbc2023d718bde
Luong Hoang <lhoang@live.com>
2016-03-03 09:56:33 -0500

    updated appmetadata and added comments

--- MultiWorldTestingServiceContract/ApplicationTransferMetadata.cs
+++ MultiWorldTestingServiceContract/ApplicationTransferMetadata.cs
@@ -6,12 +6,25 @@
 
 namespace Microsoft.Research.MultiWorldTesting.Contract
 {
+    /// <summary>
+    /// The type of decision.
+    /// </summary>
     public enum DecisionType
     {
+        /// <summary>
+        /// Choose a single action from a fixed set of available actions.
+        /// </summary>
         SingleAction = 0,
+
+        /// <summary>
+        /// Choose multiple actions from a variable set of available actions.
+        /// </summary>
         MultiActions
     }
 
+    /// <summary>
+    /// The frequency with which a new model needs to be retrained.
+    /// </summary>
     public enum TrainFrequency
     {
         Low = 0,
@@ -20,29 +33,79 @@ namespace Microsoft.Research.MultiWorldTesting.Contract
 
     public class ApplicationTransferMetadata
     {
+        /// <summary>
+        /// The name of the application as created on the Command Center.
+        /// </summary>
         public string ApplicationID { get; set; }
+
+        /// <summary>
+        /// The Id of the Azure subscription associated with this application.
+        /// </summary>
         public string SubscriptionId { get; set; }
 
+        /// <summary>
+        /// The name of the Azure resource group provisioned for this application.
+        /// </summary>
+        public string AzureResourceGroupName { get; set; }
+
+        /// <summary>
+        /// The type of decision.
+        /// </summary>
         public DecisionType DecisionType { get; set; }
 
+        /// <summary>
+        /// Number of actions available at decision time, this is only relevant if decision type is single.
+        /// </summary>
         public int? NumActions { get; set; }
 
+        /// <summary>
+        /// The training frequency type.
+        /// </summary>
         public TrainFrequency TrainFrequency { get; set; }
 
+        /// <summary>
+        /// Additional arguments to be used in training service.
+        /// </summary>
+        public string AdditionalTrainArguments { get; set; }
+
+        /// <summary>
+        /// The EventHub connection string to which the client needs to send data.
+        /// </summary>
         public string EventHubConnectionString { get; set; }
 
+        /// <summary>
+        /// The input name of the EventHub.
+        /// </summary>
         public string EventHubInputName { get; set; }
 
+        /// <summary>
+        /// The connection string for the provisioned Azure storage account.
+        /// </summary>
         public string ConnectionString { get; set; }
 
+        /// <summary>
+        /// The Id of the model to use in client library.
+        /// </summary>
         public string ModelId { get; set; }
 
+        /// <summary>
+        /// The experimental unit duration for joining decisions with observations.
+        /// </summary>
         public int ExperimentalUnitDuration { get; set; }
 
+        /// <summary>
+        /// Turn on/off exploration at client side.
+        /// </summary>
         public bool IsExplorationEnabled { get; set; }
 
+        /// <summary>
+        /// The publicly accessible Uri of the model blob that clients can check for update.
+        /// </summary>
         public string ModelBlobUri { get; set; }
 
+        /// <summary>
+        /// The publicly accessible Uri of the application settings blob that clients can check for update.
+        /// </summary>
         public string SettingsBlobUri { get; set; }
     }
 }
