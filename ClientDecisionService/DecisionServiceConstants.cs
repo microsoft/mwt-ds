4074fb437c7406bba5b9533bb94dc03ae42981c8
Markus Cozowicz <eisber@eisber.net>
2016-04-07 16:24:36 -0400

    exposing blob location...

--- ClientDecisionService/DecisionServiceConstants.cs
+++ ClientDecisionService/DecisionServiceConstants.cs
@@ -2,9 +2,10 @@
 
 namespace Microsoft.Research.MultiWorldTesting.ClientLibrary
 {
-    internal static class DecisionServiceConstants
+    public static class DecisionServiceConstants
     {
-        internal static readonly string RedirectionBlobLocation = "http://decisionservicestorage.blob.core.windows.net/app-locations/{0}";
+        // TODO: needed to run eventuploader stand alone...
+        public static readonly string RedirectionBlobLocation = "http://decisionservicestorage.blob.core.windows.net/app-locations/{0}";
         internal static readonly TimeSpan PollDelay = TimeSpan.FromSeconds(5);
     }
 }
