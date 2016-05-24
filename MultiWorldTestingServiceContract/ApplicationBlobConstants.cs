d6ed897c4624ac8b906a9d857021207b13ea4b58
Luong Hoang <lhoang@live.com>
2016-05-24 12:35:37 -0400

    updated contract and nuget version

--- MultiWorldTestingServiceContract/ApplicationBlobConstants.cs
+++ MultiWorldTestingServiceContract/ApplicationBlobConstants.cs
@@ -8,16 +8,12 @@ namespace Microsoft.Research.MultiWorldTesting.Contract
 {
     public static class ApplicationBlobConstants
     {
-        // Redirection blobs
-        public static readonly string RedirectionContainerName = "app-locations";
-        public static readonly string RedirectionBlobName = "{0}"; // resolved by token
-        
         // Model blobs
-        public static readonly string ModelContainerName = "mwt-models-{0}";
+        public static readonly string ModelContainerName = "mwt-models";
         public static readonly string LatestModelBlobName = "current";
 
         // Settings blobs
-        public static readonly string SettingsContainerName = "mwt-settings-{0}";
-        public static readonly string LatestSettingsBlobName = "settings";
+        public static readonly string SettingsContainerName = "mwt-settings";
+        public static readonly string LatestSettingsBlobName = "current";
     }
 }
