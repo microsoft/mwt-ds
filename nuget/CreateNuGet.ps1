8e852b270564533ce650e27e64910168984cc762
Markus Cozowicz <eisber@eisber.net>
2016-05-24 15:11:31 -0400

    including pdb removed gitlink dependency

--- nuget/CreateNuGet.ps1
+++ nuget/CreateNuGet.ps1
@@ -4,7 +4,7 @@ $version = Get-Content version.txt
 
 ..\packages\gitlink.2.2.0\lib\net45\GitLink.exe .. -f Decision.sln -u https://github.com/multiworldtesting/decision -p x64 -c Release -d ..\bin\x64\Release
 
-..\.nuget\nuget pack ..\ClientDecisionService\ClientDecisionService.csproj -Version $version -IncludeReferencedProjects -Prop "Configuration=Release;Platform=x64" -OutputDirectory $outputDirectory
+..\.nuget\nuget pack ..\ClientDecisionService\ClientDecisionService.nuspec -Version $version -Prop "Configuration=Release;Platform=x64" -Prop "SolutionDir=..\" -OutputDirectory $outputDirectory
 ..\.nuget\nuget pack ..\JoinServerUploader\JoinServerUploader.csproj -Version $version -Prop "Configuration=Release;Platform=x64" -OutputDirectory $outputDirectory
 ..\.nuget\nuget pack ..\MultiWorldTestingServiceContract\MultiWorldTestingServiceContract.csproj -Version $version -Prop "Configuration=Release;Platform=x64" -OutputDirectory $outputDirectory
 
