970c528ad84226fefa75f6c673fb6c6b75ce889d
Markus Cozowicz <eisber@eisber.net>
2016-06-10 14:46:23 -0400

    upgraded to VW .62

---
+++ ClientDecisionService/DcisionServiceLocal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Research.MultiWorldTesting.ClientLibrary
+{
+    public class DcisionServiceLocal<TContext>
+    {
+        public int ChooseAction(string seed, TContext context)
+        {
+            return 0;
+        }
+
+        public void Reward(TContext context, int reward)
+        {
+
+        }
+    }
+}
