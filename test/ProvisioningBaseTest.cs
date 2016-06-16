37124b544b21e5bcf1a607c53a9dea7925360981
Markus Cozowicz <eisber@eisber.net>
2016-06-16 10:47:55 -0400

    unit tests

---
+++ test/ProvisioningBaseTest.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+using Microsoft.Rest;
+using Microsoft.Azure.Management.ResourceManager;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Microsoft.Azure;
+using Microsoft.Azure.Management.ResourceManager.Models;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Configuration;
+using System.Net;
+using System.Text;
+using System.Web;
+using Microsoft.Research.MultiWorldTesting.Contract;
+
+namespace Microsoft.Research.DecisionService.Test
+{
+    public class ProvisioningBaseTest
+    {
+        private bool deleteOnCleanup;
+        private JObject deploymentOutput;
+
+        protected string managementCenterUrl;
+        protected string managementPassword;
+        protected string onlineTrainerUrl;
+        protected string onlineTrainerToken;
+        protected string webServiceUrl;
+        protected string webServiceToken;
+        protected string settingsUrl;
+
+        private static string GetConfiguration(string name)
+        {
+            var value = Environment.GetEnvironmentVariable(name);
+            if (value != null)
+                return name;
+
+            return ConfigurationManager.AppSettings[name];
+        }
+
+        protected ProvisioningBaseTest()
+        {
+            this.deleteOnCleanup = true;
+        }
+
+        protected ProvisioningBaseTest(string deploymentOutput)
+        {
+            this.deleteOnCleanup = false;
+            this.deploymentOutput = JObject.Parse(deploymentOutput);
+            this.ParseDeploymentOutputs();
+        }
+
+        protected void ConfigureDecisionService(string trainArguments = null, float? initialExplorationEpsilon = null, bool? isExplorationEnabled = null)
+        {
+            using (var wc = new WebClient())
+            {
+                wc.Headers.Add($"Authorization: {managementPassword}");
+                var query = new List<string>();
+                if (trainArguments != null)
+                    query.Add("trainArguments=" + HttpUtility.UrlEncode(trainArguments));
+                if (initialExplorationEpsilon != null)
+                    query.Add("initialExplorationEpsilon=" + initialExplorationEpsilon);
+                if (isExplorationEnabled != null)
+                    query.Add("isExplorationEnabled=" + isExplorationEnabled);
+
+                var url = managementCenterUrl + "/Automation/UpdateSettings";
+                if (query.Count > 0)
+                    url += "?" + string.Join("&", query);
+
+                wc.DownloadString(url);
+            }
+
+            // validate
+            var metaData = ApplicationMetadataUtil.DownloadMetadata<ApplicationClientMetadata>(settingsUrl);
+            if (trainArguments != null)
+                Assert.AreEqual(trainArguments, metaData.TrainArguments);
+
+            if (initialExplorationEpsilon != null)
+                Assert.AreEqual((float)initialExplorationEpsilon, metaData.InitialExplorationEpsilon);
+
+            if (isExplorationEnabled != null)
+                Assert.AreEqual((bool)isExplorationEnabled, metaData.IsExplorationEnabled);
+        }
+
+        protected void OnlineTrainerReset()
+        {
+            using (var wc = new WebClient())
+            {
+                wc.Headers.Add($"Authorization: {onlineTrainerToken}");
+                wc.DownloadString($"{onlineTrainerUrl}/reset");
+            }
+        }
+
+        private void ParseDeploymentOutputs()
+        {
+            this.managementCenterUrl = this.deploymentOutput["management Center URL"]["value"].ToObject<string>();
+            this.managementPassword = this.deploymentOutput["management Center Password"]["value"].ToObject<string>();
+            this.onlineTrainerUrl = this.deploymentOutput["online Trainer URL"]["value"].ToObject<string>();
+            this.onlineTrainerToken = this.deploymentOutput["online Trainer Token"]["value"].ToObject<string>();
+            this.webServiceUrl = this.deploymentOutput["web Service URL"]["value"].ToObject<string>();
+            this.webServiceToken = this.deploymentOutput["web Service Token"]["value"].ToObject<string>();
+            this.settingsUrl = this.deploymentOutput["client Library Url"]["value"].ToObject<string>();
+        }
+
+        private ResourceManagementClient CreateResourceManagementClient()
+        {
+            string clientId = GetConfiguration("ClientId");
+            string passwd = GetConfiguration("Password");
+            string tenantId = GetConfiguration("TenantId");
+            string subscriptionId = GetConfiguration("SubscriptionId");
+
+            var clientCredentials = new ClientCredential(clientId, passwd);
+            var authorityUri = "https://login.windows.net/" + tenantId;
+            var context = new AuthenticationContext(authorityUri);
+            var authResult = context.AcquireTokenAsync("https://management.azure.com/", clientCredentials);
+            authResult.Wait();
+
+            var tokenCredentials = new TokenCloudCredentials(subscriptionId, authResult.Result.AccessToken);
+            var credentialsForRM = new TokenCredentials(tokenCredentials.Token);
+
+            var armClient = new ResourceManagementClient(credentialsForRM);
+            armClient.SubscriptionId = subscriptionId;
+
+            return armClient;
+        }
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            // re-using deployment
+            if (this.deploymentOutput != null)
+                return;
+
+            Cleanup();
+
+            var armClient = this.CreateResourceManagementClient();
+            
+            var prefix = GetConfiguration("prefix");
+
+            var resourceGroupName = prefix + Guid.NewGuid().ToString().Replace("-","").Substring(4);
+            var deploymentName = resourceGroupName + "_deployment";
+            var location = "eastus";
+
+            // create resource group
+            var rgResult = armClient.ResourceGroups.CreateOrUpdate(resourceGroupName, new ResourceGroup { Location = location });
+
+            Assert.AreEqual("Succeeded", rgResult.Properties.ProvisioningState);
+
+            DeploymentOperation failedOperation = null;
+            using (var poller = Observable
+                .Interval(TimeSpan.FromSeconds(2))
+                .Delay(TimeSpan.FromSeconds(5))
+                .SelectMany(async _ =>
+                {
+                    try
+                    {
+                        var operations = await armClient.DeploymentOperations.ListAsync(resourceGroupName, deploymentName);
+
+                        // {op.Properties.TargetResource.Id}
+                        foreach (var op in operations)
+                        {
+                            if (op == null || op.Properties == null || op.Properties.ProvisioningState == null)
+                                continue;
+
+                            Trace.WriteLine($"Status: {op?.Properties?.TargetResource?.ResourceName,-30} '{op?.Properties?.ProvisioningState}'");
+                            switch(op.Properties.ProvisioningState)
+                            {
+                                case "Running":
+                                case "Succeeded":
+                                    break;
+                                default:
+                                    failedOperation = op;
+                                    break;
+                            }
+                        }
+                        Trace.WriteLine("");
+                    }
+                    catch (Exception ex)
+                    {
+                        Trace.WriteLine("Status polling failed: " + ex.Message);
+                    }
+
+                    return Task.FromResult(true);
+                })
+                .Subscribe())
+            {
+                var deployment = new Deployment();
+                deployment.Properties = new DeploymentProperties
+                {
+                    Mode = DeploymentMode.Incremental,
+                    TemplateLink = new TemplateLink("https://raw.githubusercontent.com/multiworldtesting/ds-provisioning/master/azuredeploy.json"),
+                    Parameters = JObject.Parse("{\"number Of Actions\":{\"value\":0}}")
+                };
+
+                try
+                {
+                    var dpResult = armClient.Deployments.CreateOrUpdate(resourceGroupName, deploymentName, deployment);
+                    this.deploymentOutput = (JObject)dpResult.Properties.Outputs;
+
+                    // make test case copy paste easy
+                    Console.WriteLine(deploymentOutput.ToString(Newtonsoft.Json.Formatting.Indented).Replace("\"", "\"\""));
+
+                    this.ParseDeploymentOutputs();
+                }
+                catch (AggregateException ae)
+                {
+                    // Go through all exceptions and dump useful information
+                    ae.Handle(x =>
+                    {
+                        Trace.WriteLine(x);
+                        return false;
+                    });
+                }
+            }
+
+            Assert.IsNull(failedOperation, $"Deployment operation failed: '{failedOperation}'");
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            if (!deleteOnCleanup)
+                return;
+
+            try
+            {
+                var armClient = this.CreateResourceManagementClient();
+                var prefix = GetConfiguration("prefix");
+                var oldResources = armClient.ResourceGroups.List().Where(r => r.Name.StartsWith(prefix));
+
+                foreach (var resource in oldResources)
+                {
+                    var task = armClient.ResourceGroups.DeleteAsync(resource.Name);
+                }
+            }
+            catch (Exception)
+            {
+            }
+        }
+    }
+}
---
+++ test/SimplePolicyTest.cs
@@ -0,0 +1,218 @@
+﻿using Microsoft.Research.MultiWorldTesting.ClientLibrary;
+using Microsoft.Research.MultiWorldTesting.Contract;
+using Microsoft.Research.MultiWorldTesting.JoinUploader;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Microsoft.Research.DecisionService.Test
+{
+    [TestClass]
+    public class SimplePolicyTestClass : ProvisioningBaseTest
+    {
+        private const string deploymentOutput = @"
+{
+  ""management Center URL"": {
+    ""type"": ""String"",
+    ""value"": ""https://mcunitd5395c8845b9951fe786e802964a-mc-dz564t66ivmi2.azurewebsites.net""
+  },
+  ""management Center Password"": {
+    ""type"": ""String"",
+    ""value"": ""4uwwocm5jwwha""
+  },
+  ""client Library Url"": {
+    ""type"": ""String"",
+    ""value"": ""https://storagedz564t66ivmi2.blob.core.windows.net/mwt-settings/client?sv=2015-04-05&sr=b&sig=3hILsSaLwF9B0ZeuAokvqEfhenv%2BJjeIxBCY4sjRHwU%3D&st=2016-06-16T13%3A37%3A00Z&se=2017-06-16T13%3A38%3A00Z&sp=r""
+  },
+  ""web Service URL"": {
+    ""type"": ""String"",
+    ""value"": ""https://mcunitd5395c8845b9951fe786e802964a-webapi-dz564t66ivmi2.azurewebsites.net""
+  },
+  ""web Service Token"": {
+    ""type"": ""String"",
+    ""value"": ""ii2wmkr677njk""
+  },
+  ""online Trainer Token"": {
+    ""type"": ""String"",
+    ""value"": ""l62oo233f3zig""
+  },
+  ""online Trainer URL"": {
+    ""type"": ""String"",
+    ""value"": ""http://mcunitd5395c8845b9951fe786e802964a-trainer-dz564t66ivmi2.cloudapp.net""
+  }
+}
+";
+
+        private Dictionary<string, int> freq;
+        private string[] features;
+        private Random rnd;
+        private int eventCount;
+
+        public SimplePolicyTestClass() : base(deploymentOutput) { }
+
+        [TestMethod]
+        public async Task SimplePolicyTest()
+        {
+            this.ConfigureDecisionService("--cb_explore 4 --epsilon 0", initialExplorationEpsilon:1, isExplorationEnabled: true);
+
+            // 4 Actions
+            var config = new DecisionServiceConfiguration(settingsUrl)
+            {
+                InteractionUploadConfiguration = new BatchingConfiguration
+                {
+                    MaxEventCount = 64
+                },
+                ObservationUploadConfiguration = new BatchingConfiguration
+                {
+                    MaxEventCount = 64
+                }
+            };
+
+            config.InteractionUploadConfiguration.ErrorHandler += JoinServiceBatchConfiguration_ErrorHandler;
+            config.InteractionUploadConfiguration.SuccessHandler += JoinServiceBatchConfiguration_SuccessHandler;
+            this.features = new string[] { "a", "b", "c", "d" };
+            this.freq = new Dictionary<string, int>();
+            this.rnd = new Random(123);
+
+            // reset the model
+            this.OnlineTrainerReset();
+
+            Console.WriteLine("Waiting after reset...");
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+
+            {
+                var expectedEvents = 0;
+                using (var client = Microsoft.Research.MultiWorldTesting.ClientLibrary.DecisionService.Create<MyContext>(config))
+                {
+                    // need to send events for at least experimental unit duration, so ASA is triggered
+                    for (int i = 0; i < 100; i++)
+                    {
+                        expectedEvents += SendEvents(client, 128);
+                        // Thread.Sleep(500);                        
+                    }
+                }
+
+                // TODO: flush doesn't work
+                // Assert.AreEqual(expectedEvents, this.eventCount);
+            }
+
+            // 4 actions times 4 feature values
+            Assert.AreEqual(4 * 4, freq.Keys.Count);
+
+            Console.WriteLine("Exploration");
+            var total = freq.Values.Sum();
+            foreach (var k in freq.Keys.OrderBy(k => k))
+            {
+                var f = freq[k] / (float)total;
+                Assert.IsTrue(f < 0.08);
+                Console.WriteLine("{0} | {1}", k, f);
+            }
+
+            freq.Clear();
+
+            // TODO: update eps: 0
+            using (var client = Microsoft.Research.MultiWorldTesting.ClientLibrary.DecisionService.Create<MyContext>(config))
+            {
+                int i;
+                for (i = 0; i < 120; i++)
+                {
+                    try
+                    {
+                        client.DownloadModelAndUpdate(new System.Threading.CancellationToken()).Wait();
+                        break;
+                    }
+                    catch (Exception)
+                    {
+                        await Task.Delay(TimeSpan.FromSeconds(1));
+                    }
+                }
+
+                Assert.IsTrue(i < 30, "Unable to download model");
+
+                for (i = 0; i < 1024; i++)
+                {
+                    var key = Guid.NewGuid().ToString();
+
+                    var featureIndex = i % features.Length;
+
+                    var action = client.ChooseAction(key, new MyContext { Feature = features[featureIndex] });
+
+                    var stat = string.Format("'{0}' '{1}' ", features[featureIndex], action);
+                    int count;
+                    if (freq.TryGetValue(stat, out count))
+                        freq[stat]++;
+                    else
+                        freq.Add(stat, count);
+                }
+            }
+
+            Console.WriteLine("Exploitation");
+            total = freq.Values.Sum();
+            foreach (var k in freq.Keys.OrderBy(k => k))
+            {
+                var f = freq[k] / (float)total;
+                Assert.AreEqual(0.25f, f, 0.1);
+                Console.WriteLine("{0} | {1}", k, f);
+            }
+        }
+
+        void JoinServiceBatchConfiguration_SuccessHandler(object source, int eventCount, int sumSize, int inputQueueSize)
+        {
+            this.eventCount += eventCount;
+        }
+
+        void JoinServiceBatchConfiguration_ErrorHandler(object source, Exception e)
+        {
+            Assert.Fail("Exception during upload: " + e.Message);
+        }
+
+        public class MyContext
+        {
+            public string Feature { get; set; }
+        }
+
+        private int SendEvents(DecisionServiceClient<MyContext> client, int numberOfEvents)
+        {
+            var expectedEvents = 0;
+
+            for (int i = 0; i < numberOfEvents; i++)
+            {
+                var key = Guid.NewGuid().ToString();
+
+                var featureIndex = i % features.Length;
+
+                var action = client.ChooseAction(key, new MyContext { Feature = features[featureIndex] });
+                expectedEvents++;
+
+                // Feature | Action
+                //    A   -->  1
+                //    B   -->  2
+                //    C   -->  3
+                //    D   -->  4
+                // only report in 50% of the cases
+                if (rnd.NextDouble() < .75 && action - 1 == featureIndex)
+                {
+                    client.ReportReward(2, key);
+                    expectedEvents++;
+                }
+
+                var stat = string.Format("'{0}' '{1}' ", features[featureIndex], action);
+                int count;
+                if (freq.TryGetValue(stat, out count))
+                    freq[stat]++;
+                else
+                    freq.Add(stat, count);
+            }
+
+            return expectedEvents;
+        }
+    }
+}
