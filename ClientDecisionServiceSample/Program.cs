6430f8d160fb9944848238212d7b5a7ca945df06
Luong Hoang <lhoang@live.com>
2016-06-17 15:42:40 -0400

    added sample code

--- ClientDecisionServiceSample/Program.cs
+++ ClientDecisionServiceSample/Program.cs
@@ -6,6 +6,8 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net;
+using System.Threading;
 
 namespace ClientDecisionServiceSample
 {
@@ -16,8 +18,14 @@ namespace ClientDecisionServiceSample
             Trace.Listeners.Add(new ConsoleTraceListener());
 
             // Sample code showing how to use the simple interface of the client library
-            // to perform news recommendation.
-            Sample1.NewsRecommendation();
+            // to perform news recommendation. This example assumes the set of topics 
+            // is constant for each decision varies.
+            Sample.NewsRecommendation();
+
+            // Sample code showing how to use the simple interface of the client library
+            // to perform news recommendation. This example assumes each topic has its 
+            // own set of features and the set of topics available for each decision varies.
+            SampleActionDependentFeature.NewsRecommendation();
         }
     }
 }
