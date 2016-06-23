a24066b77e20396eca83907e05f438add3464036
Markus Cozowicz <eisber@eisber.net>
2016-06-23 16:42:13 -0400

    simplify query

--- test/APITestDriveTest.cs
+++ test/APITestDriveTest.cs
@@ -19,30 +19,29 @@ namespace Microsoft.Research.DecisionServiceTest
 {
   ""management Center URL"": {
     ""type"": ""String"",
-    ""value"": ""https://mcdel20-mc-45amhqirwzhww.azurewebsites.net""
+    ""value"": ""https://mcdel22-mc-vzho3vjk67ppe.azurewebsites.net""
   },
   ""management Center Password"": {
     ""type"": ""String"",
-    ""value"": ""q26ifbbpulvgw""
+    ""value"": ""iab5lxbsptvp2""
   },
   ""client Library URL"": {
     ""type"": ""String"",
-    ""value"": ""https://storageftupw3x6ndhxq.blob.core.windows.net/mwt-settings/client?sv=2015-07-08&sr=b&sig=BOgAw8%2Fxk7h7Rq5Qep2k1REmcLy0KNyU8ZbMaI%2F6FQI%3D&st=2016-06-20T17%3A16%3A39Z&se=2017-06-20T17%3A17%3A39Z&sp=r""
+    ""value"": ""https://storagevzho3vjk67ppe.blob.core.windows.net/mwt-settings/client?sv=2015-07-08&sr=b&sig=hZ3O111eqfCuyLIXV4LKPR0UMiNaweKhTDEIBephGYM%3D&st=2016-06-23T20%3A19%3A19Z&se=2017-06-23T20%3A20%3A19Z&sp=r""
   },
   ""web Service Token"": {
     ""type"": ""String"",
-    ""value"": ""b4wvknzqor57w""
+    ""value"": ""wgxm34xrnhnd4""
   },
   ""online Trainer Token"": {
     ""type"": ""String"",
-    ""value"": ""t6ymqbvdphtvs""
+    ""value"": ""6n6ucxhzebevw""
   },
   ""online Trainer URL"": {
     ""type"": ""String"",
-    ""value"": ""http://mcunitc2fe50084aeb9158320f9ff760a7-trainer-ftupw3x6ndhxq.cloudapp.net""
+    ""value"": ""http://mcdel22-trainer-vzho3vjk67ppe.cloudapp.net""
   }
 }
-
 ";
         public APITestDriveTestClass() : base(deploymentOutput) { }
 
@@ -79,8 +78,8 @@ namespace Microsoft.Research.DecisionServiceTest
 
                     // mapping
                     var expectedAction = index;
-                    //if (rnd.NextDouble() < .0)
-                    //    expectedAction++;
+                    if (rnd.NextDouble() < .2)
+                        expectedAction++;
                     expectedAction = (expectedAction % 2) + 1;
 
                     var jsonContext = JsonConvert.SerializeObject(new { Location = locs[index] });
