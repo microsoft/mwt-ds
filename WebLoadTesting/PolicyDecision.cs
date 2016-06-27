ff1ca2237565302282e1df6ed6f72644f11af216
Markus Cozowicz <marcozo@microsoft.com>
2016-06-27 13:21:39 -0400

    added load test

---
+++ WebLoadTesting/PolicyDecision.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebLoadTesting
+{
+    public class PolicyDecision
+    {
+        public string EventId { get; set; }
+
+        public int Action { get; set; }
+
+        public string ModelTime { get; set; }
+    }
+}
