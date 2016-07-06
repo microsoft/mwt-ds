80eb7b301204f6f7792067b052631fe2e1f94eb1
Luong Hoang <lhoang@live.com>
2016-07-06 10:04:57 -0400

    updated ps for downloading app settings

--- mc/DownloadAppSettings.ps1
+++ mc/DownloadAppSettings.ps1
@@ -1,3 +1,3 @@
 $url = Read-Host -Prompt "Management Center URL"
 $password = Read-Host -Prompt "Password" 
-Invoke-WebRequest -Uri "$($url)/Automation/AppSettings" -Headers @{"Authorization"=$password} -OutFile ..\AppSettingsSecrets.config
+Invoke-WebRequest -Uri "$($url)/Automation/AppSettings" -Headers @{"auth"=$password} -OutFile ..\..\AppSettingsSecrets.config
