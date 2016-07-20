a94daa7088ad8b60e83374ab97d8d8c11361c0ec
Markus Cozowicz <eisber@eisber.net>
2016-07-20 16:10:22 -0400

    fixed encoding

--- forkify.ps1
+++ forkify.ps1
@@ -3,16 +3,16 @@
 
 $f = (gc provisioning\azuredeploy.json)
 $f = ($f -replace 'https://raw.githubusercontent.com/.*/mwt-ds/master/provisioning/', "https://raw.githubusercontent.com/$fork/mwt-ds/master/provisioning/")
-$f | Out-File provisioning\azuredeploy.json
+$f | Out-File -Encoding "UTF8" provisioning\azuredeploy.json
 
-$f = (gc provisioning\test\ProvisioningBaseTest.cs)
+$f = (gc provisioning\test\ProvisioningUtil.cs)
 $f = ($f -replace "https://raw.githubusercontent.com/.*/mwt-ds/master/provisioning", "https://raw.githubusercontent.com/$fork/mwt-ds/master/provisioning")
-$f | Out-File provisioning\test\ProvisioningBaseTest.cs
+$f | Out-File -Encoding "UTF8" provisioning\test\ProvisioningBaseTest.cs
 
 $f = (gc provisioning\templates\WebManageTemplate.json)
 $f = ($f -replace "https://github.com/.*/mwt-ds.git", "https://github.com/$fork/mwt-ds.git")
-$f | Out-File provisioning\templates\WebManageTemplate.json
+$f | Out-File -Encoding "UTF8" provisioning\templates\WebManageTemplate.json
 
 $f = (gc provisioning\templates\WebManageUpgradeTemplate.json)
 $f = ($f -replace "https://github.com/.*/mwt-ds.git", "https://github.com/$fork/mwt-ds.git")
-$f | Out-File provisioning\templates\WebManageUpgradeTemplate.json
+$f | Out-File -Encoding "UTF8" provisioning\templates\WebManageUpgradeTemplate.json
