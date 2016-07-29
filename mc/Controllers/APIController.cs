191686f9652c4f229d6454339c7d49fa75b27d6a
Luong Hoang <lhoang@live.com>
2016-07-29 17:04:42 -0400

    Revert "enabled cors"
    This reverts commit f6e1d4df0a4f2eee33ff472d1929d9d112a28325.

--- mc/Controllers/APIController.cs
+++ mc/Controllers/APIController.cs
@@ -1,5 +1,4 @@
-﻿using DecisionServicePrivateWeb.Attributes;
-using DecisionServicePrivateWeb.Classes;
+﻿using DecisionServicePrivateWeb.Classes;
 using Microsoft.ApplicationInsights;
 using Microsoft.Research.MultiWorldTesting.Contract;
 using Microsoft.WindowsAzure.Storage;
@@ -18,7 +17,6 @@ using VW.Serializer;
 namespace DecisionServicePrivateWeb.Controllers
 {
     [RequireHttps]
-    [AllowCrossSite]
     public class APIController : Controller
     {
         private ApplicationClientMetadata metaData;
