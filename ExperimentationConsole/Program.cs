1fb8fd4bd0dc61fd72cf8ce394c1729130f12a1f
Luong Hoang <lhoang@live.com>
2016-08-19 17:17:32 -0400

    upgraded VW, added command line sweep

--- ExperimentationConsole/Program.cs
+++ ExperimentationConsole/Program.cs
@@ -8,8 +8,10 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Globalization;
 using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Threading.Tasks.Dataflow;
@@ -109,6 +111,49 @@ namespace ExperimentationConsole
             }
 
             Console.ReadKey();
+        }
+
+        static void SweepCommandLine(string outputFile, string vwExe, string dataFile, string outModelDir, int numProcs = 30)
+        {
+            var bags = new[] { 1, 2, 4, 6, 8, 10 }.Select(a => "--bag " + a);
+            var softmaxes = new[] { 0, 1, 2, 4, 8, 16, 32 }.Select(a => "--softmax --lambda " + a);
+            var epsilons = new[] { .33333f, .2f, .1f, .05f }.Select(a => "--epsilon " + a);
+            var covers = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }.Select(a => "--cover " + a);
+
+            var arguments = Util.Expand(
+                epsilons.Union(bags).Union(softmaxes),
+                new[] { "--cb_type ips", "--cb_type mtr", "--cb_type dr" },
+                new[] { "", "-q AB -q UD" },
+                new[] { 0.005, 0.01, 0.02, 0.1, 0.2 }.Select(l => string.Format(CultureInfo.InvariantCulture, "-l {0}", l))
+            )
+            .Select((a, i) => $"--cb_explore_adf {a} -d {dataFile} -f {outModelDir}\\{i}.model -c")
+            .ToList();
+
+            File.WriteAllLines(outputFile, arguments);
+
+            for (int i = 0; i < arguments.Count; i += numProcs)
+            {
+                var processes = new List<Process>();
+                for (int j = i; j < Math.Min(i + numProcs, arguments.Count); j++)
+                {
+                    processes.Add(Process.Start(new ProcessStartInfo
+                    {
+                        FileName = vwExe,
+                        Arguments = arguments[j],
+                        RedirectStandardOutput = true,
+                        RedirectStandardError = true,
+                        UseShellExecute = false,
+                        CreateNoWindow = true
+                    }));
+                }
+                for (int j = 0; j < processes.Count; j++)
+                {
+                    string output = processes[j].StandardOutput.ReadToEnd();
+                    string error = processes[j].StandardError.ReadToEnd();
+                    File.WriteAllText($"{outModelDir}\\{i + j}.output", output + "\r\n" + error);
+                    processes[j].WaitForExit();
+                }
+            }
         }
     }
 }
