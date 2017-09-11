754fedd58e8ed4156c898e00b66e83ffab4e24c9
Markus Cozowicz <eisber@eisber.net>
2017-09-11 12:51:58 -0400

    fix trainer package version

--- mc/Controllers/DeploymentController.cs
+++ mc/Controllers/DeploymentController.cs
@@ -45,7 +45,7 @@ namespace DecisionServicePrivateWeb.Controllers
                         // Copy the .cspkg to user blob and generate SAS tokens for deployment
                         // NOTE: this has to live in blob storage, cannot be any random URL
                         // TODO: take the link URL as parameter
-                        var version = "8.4.0";
+                        var version = "8.4.0.0";
                         string cspkgLink = $"https://github.com/eisber/vowpal_wabbit/releases/download/v{version}/VowpalWabbit.Azure.{version}.{trainerSize}.cspkg";
                         var cspkgUri = ApplicationMetadataStore.CreateOnlineTrainerCspkgBlobIfNotExists(cspkgLink);
 
