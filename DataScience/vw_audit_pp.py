3f067e86bd9035b2b035f8146635685400f49682
Marco Rossi <marco.rossi@microsoft.com>
2018-11-05 15:41:09 -0500

    Fixes and small improvements
        - Added dataframe columns names
        - check_output(shell=True) is not safe
        - avoid to create .pred file in root folder and make its creation optional
        - avoid hard coded base vw command
        - debug -> verbose
        - input_vector -> log_file

--- DataScience/vw_audit_pp.py
+++ DataScience/vw_audit_pp.py
@@ -2,51 +2,48 @@
 # Parse out the vw audit logs to a dataframe.
 # ==========================================================================================
 
-import numpy as np
 import pandas as pd
-import re
-import subprocess as shell
+from subprocess import check_output
 import os
 import argparse
 
 
-# CONSTANTS
-VW_AUDIT_COMMAND = "vw -t -i {model} --dsjson {test_input} -p {test_output} -l 0.001 --audit"
-
-def run_vw_audit_command(model_file, input_vector, debug=False):
+def run_vw_audit_command(model_file, log_file, pred_file=None, verbose=False, vw_base_cmd="vw -t -l 0.001"):
     """
     Run the VW audit command for a model and input vectors.
 
     @param: model_file The input model file.
-    @param: input_vector: The input vector file.
-    @param: debug: The debug flag for advanced printing.
+    @param: log_file: The input log file.
+    @param: verbose: The verbose flag for advanced printing.
+    @param: save_pred: Flag to save model predictions file.
+    @param: vw_base_cmd: The base vw command line args.
 
     @returns: The vw audit command capture from command line.
     """
-    # Get prediction path from input vector.
-    output_file = os.path.split(input_vector)[-1] +  '.pred'
+    if pred_file:
+        # Save prediction file
+        vw_base_cmd += ' -p {pred_file}'.format(pred_file=pred_file)
+    
+    vw_audit_cmd = vw_base_cmd+' -i {model} --dsjson {test_input} --audit'.format(model=model_file, test_input=log_file)
 
-    vw_audit_cmd = VW_AUDIT_COMMAND.format(model=model_file,
-        test_input=input_vector,
-        test_output=output_file)
-
-    if (debug):
+    if verbose:
         print("VW Audit command: {0}".format(vw_audit_cmd))
     
     # Run the command in windows and check output.
-    vw_cmd_output = shell.check_output(vw_audit_cmd, shell=True).decode()    
+    vw_cmd_output = check_output(vw_audit_cmd).decode()
 
-    if (debug):
+    if verbose:
         print(vw_cmd_output)
 
     return vw_cmd_output
 
-def parse_audit_command(vw_cmd_output, audit_output_file, debug=False):
+def parse_audit_command(vw_cmd_output, audit_output_file, verbose=False):
     """
     Parse the output of the audit command and output to a dataframe.
 
-    @param: vw_cmd_output The output of the audit command.
-    @param: audit_output_file The file to which pandas data frame will be written.
+    @param: vw_cmd_output: The output of the audit command.
+    @param: audit_output_file: The file to which pandas data frame will be written.
+    @param: verbose: The verbose flag for advanced printing.
 
     @returns: The dataframe containing the audit command output.
     """
@@ -54,12 +51,12 @@ def parse_audit_command(vw_cmd_output, audit_output_file, debug=False):
     line_number = 0
     for line in [s.strip() for s in vw_cmd_output.splitlines()]:
         line_number += 1
-        if debug:
+        if verbose:
             print("Line {0}: {1}".format(line_number, line))
 
         # Process lines that have more than one tokens.
         coefficients = line.split("\t")
-        if debug:
+        if verbose:
             print("Num coefficients: {0}".format(len(coefficients)))
 
         if len(coefficients) <= 1:
@@ -69,34 +66,35 @@ def parse_audit_command(vw_cmd_output, audit_output_file, debug=False):
         # process coefficients
         for coeff in coefficients:
             coeff_term = coeff.split(":")
-            if debug:
+            if verbose:
                 print("Coefficients: {0}", coeff_term)
 
-            coeff_list.append({"term1": coeff_term[0],
-                "term2": coeff_term[1],
-                "term3": coeff_term[2],
-                "value": coeff_term[3]})
+            coeff_list.append({"Namespace^Feature": coeff_term[0],
+                               "HashValue": coeff_term[1],
+                               "FeatureValue": coeff_term[2],
+                               "WeightValue": coeff_term[3]})
 
     # Convert coefficients to pandas and output
     df_coeff = pd.DataFrame(coeff_list)
 
     # De-duplicate
-    df_coeff.set_index(["term1", "term2", "term3"], inplace=True)
+    df_coeff.set_index(["Namespace^Feature", "HashValue", "FeatureValue"], inplace=True)
     df_coeff = df_coeff[~df_coeff.index.duplicated(keep='first')]
 
     df_coeff.to_csv(audit_output_file, sep='\t', index=True)
     return df_coeff
 
-def pprint_vw_audit(model_file, input_vector, audit_output_file, debug):
+def pprint_vw_audit(model_file, log_file, audit_output_file, pred_file=None, verbose=False, vw_base_cmd='vw -t -l 0.001'):
     """
     Run the VW audit command for a model and input vectors.
 
     @param: model_file The input model file.
-    @param: input_vector: The input vector file.
-    @param: audit_output_file The file to which pandas data frame will be written.
-    @param: debug: The debug flag for advanced printing.
+    @param: log_file: The input log file.
+    @param: audit_output_file: The file to which parsed audit will be written.
+    @param: verbose: The verbose flag for advanced printing.
+    @param: save_pred: Flag to save model predictions file.
     """
-    vw_cmd_output = run_vw_audit_command(model_file, input_vector, debug)
+    vw_cmd_output = run_vw_audit_command(model_file, log_file, pred_file, verbose, vw_base_cmd)
     parse_audit_command(vw_cmd_output, audit_output_file)
 
 
@@ -105,14 +103,16 @@ if __name__ == "__main__":
 
     parser.add_argument('-m', '--model', type=str, help="VW model used for audit.", required=True)
     parser.add_argument('-i', '--input', type=str, help="input samples on which audit is to be run.", required=True)
-    parser.add_argument('-o', '--output', type=str, help="output file that captures audit in a tabular format in TSV format.", required=True)
-    parser.add_argument('-d', '--debug', type=str, required=False, help="advanced printing for debugging", default=False)
+    parser.add_argument('-o', '--output', type=str, help="output file that captures audit info in a tabular format in TSV format.", required=True)
+    parser.add_argument('-v', '--verbose', help="advanced printing for debugging", action='store_true')
+    parser.add_argument('-p', '--pred_file', type=str, help="path for output predictions file")
+    parser.add_argument('-b','--base_command', help="base Vowpal Wabbit command (default: vw -t -l 0.001)", default='vw -t -l 0.001')
 
     args = parser.parse_args()
     print(args)
 
     # Pretty print the VW audit.
-    pprint_vw_audit(args.model, args.input, args.output, args.debug)
+    pprint_vw_audit(args.model, args.input, args.output, args.pred_file, args.verbose, args.base_command)
 
     # Output message
     print("Please see {file} for VW audit output in TSV format".format(file=args.output))
