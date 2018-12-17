9d2648ce15d3e1a2f179e83940530d3f6db3150f
Marco Rossi <marco.rossi@microsoft.com>
2018-12-17 14:43:34 -0800

    ds_parse.py: Improvements - Not requiring "Version" field to be present in json_cooked() - Only assuming checkpoint line starts with [ - Reuse extract_field() - Enabled progressbar when total is unknown

--- DataScience/ds_parse.py
+++ DataScience/ds_parse.py
@@ -1,10 +1,13 @@
 import time,collections,os,types,gzip,sys
 
-def update_progress(current, total, prefix=''):
-    barLength = 50 # Length of the progress bar
-    progress = current/total
-    block = int(barLength*progress)
-    text = "\r{}Progress: [{}] {:.1f}%".format(prefix, "#"*block + "-"*(barLength-block), progress*100)
+def update_progress(current, total=None, prefix=''):
+    if total:
+        barLength = 50 # Length of the progress bar
+        progress = current/total
+        block = int(barLength*progress)
+        text = "\r{}Progress: [{}] {:.1f}%".format(prefix, "#"*block + "-"*(barLength-block), progress*100)
+    else:
+        text = "\r{}Iter: {}".format(prefix, current)
     sys.stdout.write(text)
     sys.stdout.flush()
     return len(text)-1      # return length of text string not counting the initial \r
@@ -45,10 +48,13 @@ def process_dsjson_file(fp, d=None, e=None):
     with (gzip.open(fp, 'rb') if fp.endswith('.gz') else open(fp, 'rb')) as file_input:
         for i,x in enumerate(file_input):
             bytes_count += len(x)
-            if (i+1) % 1000 == 0 and not fp.endswith('.gz'):
-                update_progress(bytes_count,tot_bytes, fp+' - ')
+            if (i+1) % 1000 == 0:
+                if fp.endswith('.gz'):
+                    update_progress(i+1,prefix=fp+' - ')
+                else:
+                    update_progress(bytes_count,tot_bytes,fp+' - ')
                 
-            if x.startswith(b'[{"Partition'):   # Ignore checkpoint info line
+            if x.startswith(b'['):   # Ignore checkpoint info line
                 continue
 
             if not (x.startswith(b'{"') or x.strip().endswith(b'}')):
@@ -97,7 +103,10 @@ def process_dsjson_file(fp, d=None, e=None):
                 e_c += 1
                 e_s.add(data['ei'])
 
-        len_text = update_progress(bytes_count,tot_bytes, fp+' - ')
+        if fp.endswith('.gz'):
+            len_text = update_progress(i+1,prefix=fp+' - ')
+        else:
+            len_text = update_progress(bytes_count,tot_bytes, fp+' - ')
         sys.stdout.write("\r" + " "*len_text + "\r")
         sys.stdout.flush()
     return stats, d_s, e_s, d_c, e_c, slot_len_c, rew_multi_a, baselineRandom, not_activated
@@ -122,28 +131,32 @@ def json_cooked(x, do_devType=False, do_VWState=False, do_p_vec=False):
     # {"_label_cost":0,"_label_probability":0.01818182,"_label_Action":9,"_labelIndex":8,"Timestamp":"2017-10-24T00:00:15.5160000Z","Version":"1","EventId":"fa68cd9a71764118a635fd3d7a908634","a":[9,11,3,1,6,4,10,5,7,8,2],"c":{"_synthetic":false,"User":{"_age":0},"Geo":{"country":"United States","_countrycf":"8","state":"New York","city":"Springfield Gardens","_citycf":"8","dma":"501"},"MRefer":{"referer":"http://www.complex.com/"},"OUserAgent":{"_ua":"Mozilla/5.0 (iPad; CPU OS 10_3_2 like Mac OS X) AppleWebKit/603.2.4 (KHTML, like Gecko) Version/10.0 Mobile/14F89 Safari/602.1","_DeviceBrand":"Apple","_DeviceFamily":"iPad","_DeviceIsSpider":false,"_DeviceModel":"iPad","_OSFamily":"iOS","_OSMajor":"10","_OSPatch":"2","DeviceType":"Tablet"},"_multi":[{"
     # {"_label_cost":0,"_label_probability":1,"_label_Action":1,"_labelIndex":0,"_deferred":true,"Timestamp":"2018-10-25T00:01:31.1780000Z","Version":"1","EventId":"28EF7EE0B9CF4E319696CB812973F0B3","DeferredAction":true,"a":[1],"c":{"Global":{"SLOT":"1"},"Request":{"DISPLOC":"BR"},"Profile":{"_REQASID":"F2A3670E24F94646983BEB3EB35CB0C9","COUNTRY":"BR","FPIGM":"FALSE","FPNIGM":"FALSE","FB":"FALSE"},"_multi":[{"Action":{"constant":1,"PayloadID":"425039838"}}]},"p":[1.000000],"VWState":{"m":"DF30E6A3648947E69EE6B0816BF42640/1A5CD300DFE546B7B29A11EB70980809"}}
     # {"_label_cost":0,"_label_probability":0.833333015,"_label_Action":6,"_labelIndex":5,"o":[{"EventId":"A3E5ADF82D3A4BD5A5161FFC19C95DBB","DeferredAction":false}],"Timestamp":"2018-10-25T00:00:00.3960000Z","Version":"1","EventId":"A3E5ADF82D3A4BD5A5161FFC19C95DBB","DeferredAction":true,"a":[6,2,4,5,1,3],"c":{"Global":{"SLOT":"2"},"Request":{"DISPLOC":"US"},"Profile":{"_REQASID":"70A03B1FD0CA4B5A89669637DD448161","COUNTRY":"US","FPIGM":"FALSE","FPNIGM":"FALSE","FB":"FALSE","F1":0.0,"F2":0.0,"F3":0.00157480315,"F4":0.00182481752,"F5":0.0,"F6":0.00136892539,"F7":0.0,"F8":-1.0,"F9":0.0434782609,"F11":0.0,"F12":0.0,"F13":0.0,"F14":1.0,"F15":1.0,"F16":0.0,"F17":2.0,"F18":0.0,"F19":0.0,"F20":1.0
-    # Assumption: "Version" value is 1 digit
+    # Assumption: "Version" value is 1 digit string
     #
     # Performance: 4x faster than Python JSON parser js = json.loads(x.strip())
     #################################
     ind1 = x.find(b',',16)              # equal to: x.find(',"_label_prob',16)
     ind2 = x.find(b',',ind1+23)         # equal to: x.find(',"_label_Action',ind1+23)
-    ind4 = x.find(b',"T',ind2+34)       # equal to: x.find(',"Timestamp',ind2+34)
-    ind5 = x.find(b'"',ind4+36)         # equal to: x.find('","Version',ind4+36)
-    ind6 = x.find(b'"',ind5+27)
-    ind7 = x.find(b',"a"',ind5+28)
+    ind3 = x.find(b',"T',ind2+34)       # equal to: x.find(',"Timestamp',ind2+34)
+    ind4 = x.find(b'"',ind3+33)
+    if x[ind4+3:ind4+10] == b'Version': # check for the presence of "Version":"1"
+        ind5 = ind4+27                  # len('","Version":"1","EventId":"') = 27
+    else:
+        ind5 = ind4+13                  # len('","EventId":"') = 13
+    ind6 = x.find(b'"',ind5)
+    ind7 = x.find(b',"a"',ind5+16)
     ind8 = x.find(b']',ind7+7)          # equal to: x.find('],"c',ind7+8)
 
     data = {}
     data['o'] = 1 if b',"o":' in x[ind2+30:ind2+50] else 0
     data['cost'] = x[15:ind1]                   # len('{"_label_cost":') = 15
     data['p'] = float(x[ind1+22:ind2])          # len(',"_label_probability":') = 22
-    data['ts'] = x[ind4+14:ind5]                # len(',"Timestamp":"') = 14
-    data['ei'] = x[ind5+27:ind6]                # len('","Version":"1","EventId":"') = 27
-    data['a_vec'] = x[ind7+6:ind8].split(b',')  # len('","a":[') = 7
+    data['ts'] = x[ind3+14:ind4]                # len(',"Timestamp":"') = 14
+    data['ei'] = x[ind5:ind6]
+    data['a_vec'] = x[ind7+6:ind8].split(b',')  # len(',"a":[') = 6
     data['a'] = int(data['a_vec'][0])
     data['num_a'] = len(data['a_vec'])
-    data['skipLearn'] = b'"_skipLearn":true' in x[ind2+30:ind2+55]
+    data['skipLearn'] = b'"_skipLearn":true' in x[ind2+34:ind3] # len('"_label_Action":1,"_labelIndex":0,') = 34
     
     if do_VWState:
         ind11 = x[-120:].find(b'VWState')
@@ -153,12 +166,7 @@ def json_cooked(x, do_devType=False, do_VWState=False, do_p_vec=False):
         data['p_vec'] = [float(p) for p in extract_field(x[-120-15*data['num_a']:], b'"p":[', b']').split(b',')]
         
     if do_devType:
-        ind9 = x.find(b'"DeviceType',ind8)
-        if ind9 > -1:
-            ind10 = x.find(b'"},"_mul', ind9+15)
-            data['devType'] = x[ind9+14:ind10]   # len('"DeviceType":"') = 14
-        else:
-            data['devType'] = b'N/A'
+        data['devType'] = extract_field(x[ind8:],b'"DeviceType":"',b'"')
             
     return data
 
@@ -195,7 +203,10 @@ def json_dangling(x):
     return data
 
 def extract_field(x,sep1,sep2,space=1):
-    ind1 = x.find(sep1)+len(sep1)
+    ind1 = x.find(sep1)
+    if ind1 < 0:
+        return b'N/A'
+    ind1 += len(sep1)
     ind2 = x.find(sep2,ind1+space)
     if ind2 == -1:
         return x[ind1:]
