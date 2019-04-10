7b3194fa6bf9a6401af8b4d1407abbd085469d68
Dwaipayan Mukherjee <dwaijam@gmail.com>
2019-04-09 17:52:28 -0700

    add ,

--- DataScience/Experimentation.py
+++ DataScience/Experimentation.py
@@ -179,7 +179,7 @@ def generate_predictions_files(log_fp, policies):
         policy_command =  policy.full_command.replace('--cb_adf', '--cb_explore_adf --epsilon 0.2')
         data['policies'].append({
             'name':name,
-            'arguments': re.sub(r'-c\s', '', re.sub(r'-d\s[\S]*\s', '', policy_command))
+            'arguments': re.sub(r'-c\s', '', re.sub(r'-d\s[\S]*\s', '', policy_command)),
             'loss': policy.loss
             })
         print('Name: {} Ave. Loss: {} cmd: {}'.format(name, policy.loss, policy_command))
