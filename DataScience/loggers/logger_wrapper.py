3801a772b63812b99149c6304e305b77abfc793e
Lily Motley <limotley@microsoft.com>
2020-07-13 10:01:02 -0700

    Removed GenevaLogger

--- DataScience/loggers/logger_wrapper.py
+++ DataScience/loggers/logger_wrapper.py
@@ -12,30 +12,30 @@ class Logger:
     def create_loggers(cls, geneva=False, **geneva_args):
         Logger.loggers.append(ConsoleLogger())
         if geneva:
-            Logger.loggers.append(GenevaLogger(**geneva_args))
+            cls.loggers.append(GenevaLogger(**geneva_args))
 
     @classmethod
     def info(cls, msg: str, **kwargs):
-        for logger in Logger.loggers:
+        for logger in cls.loggers:
             logger.info(msg, **kwargs)
 
     @classmethod
     def warning(cls, msg: str, **kwargs):
-        for logger in Logger.loggers:
+        for logger in cls.loggers:
             logger.warning(msg, **kwargs)
 
     @classmethod
     def error(cls, msg: str, **kwargs):
-        for logger in Logger.loggers:
+        for logger in cls.loggers:
             logger.error(msg, **kwargs)
 
     @classmethod
     def exception(cls, msg: str='', **kwargs):
-        for logger in Logger.loggers:
+        for logger in cls.loggers:
             logger.exception(msg, **kwargs)
 
     @classmethod
     def close(cls):
-        for logger in Logger.loggers:
+        for logger in cls.loggers:
             logger.close()
 
